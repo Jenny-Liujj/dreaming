abd622a2f3dd95aa6b286d1f33280a3feed53042
刘精精 <liujingjing7899@gmail.com>
2016-07-06 17:52:31 +0800

    fdfffd

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,10 +8,18 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :deploy_to, '/home/www/dreaming'
 set :scm, :git
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml','config/settings.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :keep_releases, 15
+
+
+namespace :deploy do
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+
+end
 
 namespace :db do
   desc 'Create database if not exist.'
