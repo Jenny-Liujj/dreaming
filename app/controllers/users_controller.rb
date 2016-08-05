8b6bbcca4031851c1b783ba2e1d2b1ff7d7a8872
刘精精 <liujingjing7899@gmail.com>
2016-08-05 16:14:32 +0800

    ww

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :current_user, except: [:create]
 
+  def index
 
+  end
 
   def create
     respond_to do |format|
       if params[:user][:name] == "tianyuan"
+        session[:user_id] = 'tianyuan'
         format.html{redirect_to users_path, alert:'欢迎您，亲爱的，'}
       else
         format.html{redirect_to :back,alert:'请重新输入秘密'}
@@ -13,13 +17,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name)
     end
